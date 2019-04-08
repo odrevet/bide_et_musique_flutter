42660c186ad78158f3bad4b319ad3e24876b4813
odrevet <odrevet@gmail.com>
2019-04-08 12:11:35 +0200

    song page now display the cover. Added player for the song (todo : fix conflict with radio stream player)

--- lib/song.dart
+++ lib/song.dart
@@ -1,7 +1,9 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:html/dom.dart' as dom;
 import 'package:html/parser.dart' as parser;
+import 'package:audioplayer/audioplayer.dart';
 import 'utils.dart';
 
 class Song {
@@ -86,12 +88,160 @@ class SongPageWidget extends StatelessWidget {
 
   Widget _buildView(String lyrics) {
     return new Container(
-      child: SingleChildScrollView(
-          child: Text(lyrics,
-              style: new TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.black,
-                  fontWeight: FontWeight.w600))),
+      child: Center(
+          child: Column(
+        children: <Widget>[
+          Expanded(
+              flex: 3,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(
+                      child: Container(
+                    decoration: new BoxDecoration(
+                        image: new DecorationImage(
+                      fit: BoxFit.fill,
+                      alignment: FractionalOffset.topCenter,
+                      image: new NetworkImage(
+                          'http://www.bide-et-musique.com/images/pochettes/' +
+                              song.id +
+                              '.jpg'),
+                    )),
+                  )),
+                  Expanded(
+                    child: Text('TODO'),//SongPlayerWidget(song.id),
+                  ),
+                ],
+              )),
+          Expanded(
+            flex: 7,
+            child: SingleChildScrollView(
+                child: Text(lyrics,
+                    style: new TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w600))),
+          ),
+        ],
+      )),
     );
+  }
+}
+
+////////////////////////////////
+enum PlayerState { stopped, playing, paused }
+
+class SongPlayerWidget extends StatefulWidget {
+  final String _songId;
+  SongPlayerWidget(this._songId, {Key key}) : super(key: key);
+
+  @override
+  _SongPlayerWidgetState createState() => _SongPlayerWidgetState(this._songId);
+}
+
+class _SongPlayerWidgetState extends State<SongPlayerWidget> {
+  final String _songId;
+
+  Duration duration;
+  Duration position;
+  AudioPlayer audioPlayer;
+  PlayerState playerState = PlayerState.stopped;
+
+  get isPlaying => playerState == PlayerState.playing;
+  get isPaused => playerState == PlayerState.paused;
+
+  StreamSubscription _audioPlayerStateSubscription;
+
+  _SongPlayerWidgetState(this._songId);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            new Material(child: _buildPlayer()),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPlayer() {
+    var playStopButton;
+    if (isPlaying) {
+      playStopButton = new IconButton(
+          onPressed: isPlaying || isPaused ? () => stop() : null,
+          iconSize: 80.0,
+          icon: new Icon(Icons.stop),
+          color: Colors.orange);
+    } else {
+      playStopButton = new IconButton(
+          onPressed: isPlaying ? null : () => play(),
+          iconSize: 80.0,
+          icon: new Icon(Icons.play_arrow),
+          color: Colors.orange);
+    }
+
+    return new Container(
+        padding: new EdgeInsets.all(16.0),
+        child: new Column(mainAxisSize: MainAxisSize.min, children: [
+          new Row(mainAxisSize: MainAxisSize.min, children: [playStopButton]),
+        ]));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initAudioPlayer();
+  }
+
+  @override
+  void dispose() {
+    _audioPlayerStateSubscription.cancel();
+    audioPlayer.stop();
+    super.dispose();
+  }
+
+  void onComplete() {
+    setState(() => playerState = PlayerState.stopped);
+  }
+
+  void initAudioPlayer() {
+    audioPlayer = new AudioPlayer();
+    _audioPlayerStateSubscription =
+        audioPlayer.onPlayerStateChanged.listen((s) {
+      if (s == AudioPlayerState.PLAYING) {
+        setState(() => duration = audioPlayer.duration);
+      } else if (s == AudioPlayerState.STOPPED) {
+        onComplete();
+        setState(() {
+          position = duration;
+        });
+      }
+    }, onError: (msg) {
+      setState(() {
+        playerState = PlayerState.stopped;
+        duration = new Duration(seconds: 0);
+        position = new Duration(seconds: 0);
+      });
+    });
+  }
+
+  Future play() async {
+    await audioPlayer
+        .play('http://www.bide-et-musique.com/stream_' + this._songId + '.php');
+    setState(() {
+      playerState = PlayerState.playing;
+    });
+  }
+
+  Future stop() async {
+    await audioPlayer.stop();
+    setState(() {
+      playerState = PlayerState.stopped;
+      position = new Duration();
+    });
   }
 }
