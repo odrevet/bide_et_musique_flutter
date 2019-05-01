42d2fa913d5ba63622ea3bd4b03aea5eefe8cf15
odrevet <odrevet@gmail.com>
2019-05-01 07:34:41 +0200

    confirmation before remove song from favourites

--- lib/account.dart
+++ lib/account.dart
@@ -236,53 +236,97 @@ class _ManageAccountWidgetState extends State<ManageAccountWidget> {
     _rows = <Dismissible>[];
   }
 
+  Dismissible _createSongTile(Song song, AccountInformations accountInformations){
+    return Dismissible(
+        key: Key(song.id),
+        onDismissed: (direction) {
+          _confirmDeletion(song, accountInformations);
+        },
+        child: ListTile(
+          leading: CircleAvatar(
+            backgroundColor: Colors.black12,
+            child: Image(
+                image:
+                NetworkImage('$baseUri/images/thumb25/${song.id}.jpg')),
+          ),
+          title: Text(
+            song.title,
+          ),
+          subtitle: Text(song.artist),
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => SongPageWidget(
+                        song: song,
+                        songInformations: fetchSongInformations(song.id))));
+          },
+        ));
+  }
+
+  Future<void> _confirmDeletion(Song song, AccountInformations accountInformations) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Retirer un favoris'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text('Vraiment retirer cette chanson de vos favoris ? '),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Oui'),
+              onPressed: () async {
+                var accountId = session.id;
+                var K = song.id;
+                var direction = 'DS';
+
+                final response = await session.post(
+                    '$baseUri/account/$accountId.html', {
+                  'K': K,
+                  'Step': '',
+                  direction + '.x': '1',
+                  direction + '.y': '1'
+                });
+
+                if (response.statusCode == 200) {
+                  setState(() {
+                    accountInformations.favorites
+                        .removeWhere((song) => song.id == K);
+                  });
+                }
+                Navigator.of(context).pop();
+              },
+            ),
+            FlatButton(
+              child: Text('Non'),
+              onPressed: () {
+                int index = accountInformations.favorites.indexOf(song);
+
+                setState(() {
+                  _rows.insert(index, _createSongTile(song, accountInformations));
+                });
+
+
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   Widget _buildView(BuildContext context, Session session,
       AccountInformations accountInformations) {
     _rows.clear();
     for (Song song in accountInformations.favorites) {
-      _rows.add(Dismissible(
-          key: Key(song.id),
-          onDismissed: (direction) async {
-            var accountId = session.id;
-            var K = song.id;
-            var direction = 'DS';
-
-            final response = await session.post(
-                '$baseUri/account/$accountId.html', {
-              'K': K,
-              'Step': '',
-              direction + '.x': '1',
-              direction + '.y': '1'
-            });
-
-            if (response.statusCode == 200) {
-              setState(() {
-                //update model
-                accountInformations.favorites
-                    .removeWhere((song) => song.id == K);
-              });
-            }
-          },
-          child: ListTile(
-            leading: CircleAvatar(
-              backgroundColor: Colors.black12,
-              child: Image(
-                  image:
-                      NetworkImage('$baseUri/images/thumb25/${song.id}.jpg')),
-            ),
-            title: Text(
-              song.title,
-            ),
-            subtitle: Text(song.artist),
-            onTap: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => SongPageWidget(
-                          song: song,
-                          songInformations: fetchSongInformations(song.id))));
-            },
-          )));
+      _rows.add(_createSongTile(song, accountInformations));
     }
 
     return ReorderableListView(
