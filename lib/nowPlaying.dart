9a873ce7c79972a3687e84ccc820449b7a2a9048
Olivier Drevet <odrevet@gmail.com>
2020-03-14 23:18:44 +0100

    use wapi to fetch now playing song

--- lib/nowPlaying.dart
+++ lib/nowPlaying.dart
@@ -1,41 +1,34 @@
 import 'dart:async';
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:html/dom.dart' as dom;
-import 'package:html/parser.dart' as parser;
+
 
 import 'session.dart';
 import 'song.dart';
 import 'utils.dart';
 
-Future<SongLink> fetchNowPlaying() async {
-  var songLink = SongLink();
-  final url = '$baseUri/now-top.php';
-  final response = await Session.get(url);
+Future<Song> fetchNowPlaying() async {
+  final url = '$baseUri/wapi/song/now';
+  final responseJson = await Session.get(url);
 
-  if (response.statusCode == 200) {
-    var body = response.body;
-    dom.Document document = parser.parse(body);
-
-    var titreSong = document.getElementsByClassName('titre-song')[0];
-    String href = titreSong.children[0].attributes['href'];
-    songLink.id = extractSongId(href);
-    songLink.title = stripTags(titreSong.children[0].innerHtml);
-
-    var titreSong2 = document.getElementsByClassName('titre-song2')[0];
-    songLink.artist = stripTags(titreSong2.children[0].innerHtml);
-
-    var request = document.getElementById('requete');
-    songLink.program = stripTags(request.innerHtml);
-    return songLink;
+  if (responseJson.statusCode == 200) {
+    try {
+      String decodedString = utf8.decode(responseJson.bodyBytes);
+      Map<String, dynamic> decodedJson = json.decode(decodedString);
+      return Song.fromJson(decodedJson);
+    } catch (e) {
+      print('ERROR $e');
+    }
   } else {
-    throw Exception('Failed to load top');
+    print('Response was ${responseJson.statusCode}');
   }
 }
 
 class NowPlayingWidget extends StatefulWidget {
-  Future<SongLink> _songLink;
-  NowPlayingWidget(this._songLink, {Key key}) : super(key: key);
+  Future<Song> _song;
+
+  NowPlayingWidget(this._song, {Key key}) : super(key: key);
 
   @override
   _NowPlayingWidgetState createState() => _NowPlayingWidgetState();
@@ -47,8 +40,8 @@ class _NowPlayingWidgetState extends State<NowPlayingWidget> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: FutureBuilder<SongLink>(
-        future: widget._songLink,
+      child: FutureBuilder<Song>(
+        future: widget._song,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
             return SongCardWidget(songLink: snapshot.data);
@@ -60,9 +53,10 @@ class _NowPlayingWidgetState extends State<NowPlayingWidget> {
                   errorDisplay(snapshot.error),
                   RaisedButton.icon(
                     icon: Icon(Icons.refresh),
-                    onPressed: () => setState(() {
-                      widget._songLink = fetchNowPlaying();
-                    }),
+                    onPressed: () =>
+                        setState(() {
+                          widget._song = fetchNowPlaying();
+                        }),
                     label: Text('Ré-essayer maintenant'),
                   )
                 ]);
