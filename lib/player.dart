d1e8ddf38ce8f7530b147317ae661065d3809cf5
Olivier Drevet <odrevet@gmail.com>
2020-06-04 23:51:44 +0200

    broadcast ICY metadata

--- lib/player.dart
+++ lib/player.dart
@@ -126,6 +126,7 @@ class AudioPlayerTask extends BackgroundAudioTask {
             processingState: bufferingState ?? AudioProcessingState.ready,
             position: event.position,
           );
+          AudioServiceBackground.sendCustomEvent(event.icyMetadata);
           break;
         case AudioPlaybackState.connecting:
           _setState(
