95504c58fd396942ab3c35632ffd6374019feba0
odrevet <odrevet@gmail.com>
2020-12-09 22:26:32 +0100

    Cover can be viewved in 3D and with Interactive (zoom)

--- lib/widgets/cover_viewer.dart
+++ lib/widgets/cover_viewer.dart
@@ -17,27 +17,38 @@ class CoverViewer extends StatefulWidget {
 class _CoverViewerState extends State<CoverViewer> {
   SongLink songLink;
   Offset _offset = Offset.zero;
+  var _3dMode = false; // 3D or zoom
 
   _CoverViewerState(this.songLink);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(songLink.name),
-      ),
-      body: Center(
-        child: Transform(
-            transform: Matrix4.identity()
-              ..setEntry(3, 2, 0.001)
-              ..rotateX(0.01 * _offset.dy)
-              ..rotateY(-0.01 * _offset.dx),
-            alignment: FractionalOffset.center,
+      appBar: AppBar(title: Text(songLink.name), actions: <Widget>[
+        Padding(
+            padding: EdgeInsets.only(right: 20.0),
             child: GestureDetector(
-              onPanUpdate: (details) => setState(() => _offset += details.delta),
-              onDoubleTap: () => setState(() => _offset = Offset.zero),
-              child: _buildView(context),
+              onTap: () => setState(() => _3dMode = !_3dMode),
+              child: Icon(_3dMode ? Icons.threed_rotation : Icons.zoom_out_map ),
             )),
+      ]),
+      body: Center(
+        child: _3dMode
+            ? Transform(
+                transform: Matrix4.identity()
+                  ..setEntry(3, 2, 0.001)
+                  ..rotateX(0.01 * _offset.dy)
+                  ..rotateY(-0.01 * _offset.dx),
+                alignment: FractionalOffset.center,
+                child: GestureDetector(
+                  onPanUpdate: (details) =>
+                      setState(() => _offset += details.delta),
+                  onDoubleTap: () => setState(() => _offset = Offset.zero),
+                  child: _buildView(context),
+                ))
+            : InteractiveViewer(
+                child: _buildView(context),
+              ),
       ),
     );
   }
