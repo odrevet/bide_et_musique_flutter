0fdc0d0e52452c45dca3f2236accc001369ac2a0
odrevet <odrevet@gmail.com>
2021-11-01 10:57:01 +0100

    embed let encrypt r3 cert

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'dart:async';
+import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:audio_service/audio_service.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'player.dart';
 import 'widgets/bide_app.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  ByteData data = await PlatformAssetBundle().load('assets/lets-encrypt-r3.pem');
+  SecurityContext.defaultContext.setTrustedCertificatesBytes(data.buffer.asUint8List());
+
   audioHandler = await AudioService.init(
     builder: () => AudioPlayerHandler(),
     config: const AudioServiceConfig(
