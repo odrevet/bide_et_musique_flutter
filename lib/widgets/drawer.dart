bb8a2eaee44131748c99fd3931290636c7a0c2d0
odrevet <odrevet@gmail.com>
2022-01-24 09:08:51 +0100

    package_info_plus

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:package_info/package_info.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 import '../models/song.dart';
 import '../services/account.dart';
