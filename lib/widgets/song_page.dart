ec168db9a132d1eac6e7163c4c6d19ad1d0dac57
odrevet <odrevet@gmail.com>
2024-08-04 09:23:46 +0200

    wip: split song page into widgets

--- lib/widgets/song_page.dart
+++ lib/widgets/song_page.dart
@@ -17,6 +17,201 @@ import 'html_with_style.dart';
 import 'song_app_bar.dart';
 import 'song_information.dart';
 
+class SongPageNestedScrollView extends StatefulWidget {
+  final SongLink songLink;
+  final Song song;
+  final Function onPageChange;
+  final TextEditingController commentController;
+
+  const SongPageNestedScrollView(
+      this.songLink, this.song, this.onPageChange, this.commentController,
+      {super.key});
+
+  @override
+  State<SongPageNestedScrollView> createState() =>
+      _SongPageNestedScrollViewState();
+}
+
+class _SongPageNestedScrollViewState extends State<SongPageNestedScrollView> {
+  final PageController _pageController = PageController(
+    initialPage: 0,
+  );
+
+  Widget _buildViewComments(BuildContext context, Song song) {
+    List<Comment> comments = song.comments;
+    var rows = <Widget>[];
+    String? loginName = Session.accountLink.name;
+    var selfComment = const TextStyle(
+      color: Colors.red,
+    );
+
+    editMessageDialog(BuildContext context, Song song, Comment comment) {
+      widget.commentController.text = comment.body!;
+      return showDialog<void>(
+        context: context,
+        barrierDismissible: true,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            actions: [
+              ElevatedButton.icon(
+                icon: const Icon(Icons.send),
+                label: const Text("Envoyer"),
+                onPressed: () async {
+                  Navigator.of(context).pop();
+                  sendEditComment(song, comment, widget.commentController.text);
+                  // refresh current page so posted comment is visible
+                  Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => SongPageWidget(
+                              songLink: widget.songLink,
+                              song: fetchSong(widget.songLink!.id))));
+                },
+              )
+            ],
+            title: const Text('Edition d\'un commentaire'),
+            content: TextFormField(
+                maxLines: 5,
+                controller: widget.commentController,
+                decoration: const InputDecoration(
+                  hintText: 'Entrez votre commentaire ici',
+                )),
+          );
+        },
+      );
+    }
+
+    for (Comment comment in comments) {
+      rows.add(ListTile(
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) =>
+                        AccountPage(account: fetchAccount(comment.author.id))));
+          },
+          title: HtmlWithStyle(data: comment.body),
+          subtitle: Text('Par ${comment.author.name!} ${comment.time}',
+              style: comment.author.name == loginName ? selfComment : null),
+          trailing: comment.author.name == loginName
+              ? IconButton(
+                  icon: const Icon(Icons.edit),
+                  onPressed: () async {
+                    editMessageDialog(context, song, comment);
+                  },
+                )
+              : null));
+      rows.add(const Divider());
+    }
+
+    return ListView(children: rows);
+  }
+
+  void _openCoverViewerDialog(SongLink? songLink, BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          return CoverViewer(songLink);
+        },
+        fullscreenDialog: true));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final String coverLink = widget.song.coverLink;
+    final tag = createTag(widget.songLink);
+
+    return NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverAppBar(
+              backgroundColor: Theme.of(context).canvasColor,
+              expandedHeight: 200.0,
+              automaticallyImplyLeading: false,
+              floating: true,
+              flexibleSpace: FlexibleSpaceBar(
+                  background: Row(children: [
+                Expanded(
+                    flex: 3,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Expanded(
+                          flex: 1,
+                          child: Padding(
+                              padding: const EdgeInsets.all(5.0),
+                              child: Hero(
+                                  tag: tag,
+                                  child: Material(
+                                    child: InkWell(
+                                        onTap: () {
+                                          _openCoverViewerDialog(
+                                              widget.songLink, context);
+                                        },
+                                        child: CachedNetworkImage(
+                                            imageUrl: coverLink)),
+                                  ))),
+                        ),
+                        Expanded(
+                            flex: 1,
+                            child: SingleChildScrollView(
+                                child: SongInformations(song: widget.song))),
+                      ],
+                    ))
+              ])),
+            ),
+          ];
+        },
+        body: Stack(children: [
+          CachedNetworkImage(
+            imageUrl: widget.song.coverLink,
+            imageBuilder: (context, imageProvider) => Container(
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: imageProvider, fit: BoxFit.cover)),
+                child: BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 9.6, sigmaY: 9.6),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.grey.shade200.withOpacity(0.7)),
+                  ),
+                )),
+          ),
+          Stack(
+            children: [
+              PageView(
+                controller: _pageController,
+                onPageChanged: (int page) => widget.onPageChange(page),
+                children: <Widget>[
+                  widget.song.lyrics != null
+                      ? SingleChildScrollView(
+                          child: Padding(
+                          padding: const EdgeInsets.only(left: 4.0, top: 2.0),
+                          child: HtmlWithStyle(
+                              data: widget.song.lyrics == ''
+                                  ? '<i>Paroles non renseignées</i>'
+                                  : widget.song.lyrics),
+                        ))
+                      : const Center(child: CircularProgressIndicator()),
+                  _buildViewComments(context, widget.song),
+                ],
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: PageIndicator(
+                  controller: _pageController,
+                  count: 2,
+                  size: 10.0,
+                  layout: PageIndicatorLayout.WARM,
+                  scale: 0.75,
+                  space: 10,
+                ),
+              ),
+            ],
+          ),
+        ]));
+  }
+}
+
 class SongPageWidget extends StatefulWidget {
   final SongLink? songLink;
   final Future<Song>? song;
@@ -30,9 +225,6 @@ class SongPageWidget extends StatefulWidget {
 class _SongPageWidgetState extends State<SongPageWidget> {
   int? _currentPage;
   final _commentController = TextEditingController();
-  final PageController _pageController = PageController(
-    initialPage: 0,
-  );
 
   _SongPageWidgetState();
 
@@ -54,14 +246,6 @@ class _SongPageWidgetState extends State<SongPageWidget> {
         return _buildView(context, song);
       },
     );
-  }
-
-  void _openCoverViewerDialog(SongLink? songLink, BuildContext context) {
-    Navigator.of(context).push(MaterialPageRoute<void>(
-        builder: (BuildContext context) {
-          return CoverViewer(songLink);
-        },
-        fullscreenDialog: true));
   }
 
   _newMessageDialog(BuildContext context, Song song) {
@@ -104,137 +288,13 @@ class _SongPageWidgetState extends State<SongPageWidget> {
     );
   }
 
-  _editMessageDialog(BuildContext context, Song song, Comment comment) {
-    _commentController.text = comment.body!;
-    return showDialog<void>(
-      context: context,
-      barrierDismissible: true,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          actions: [
-            ElevatedButton.icon(
-              icon: const Icon(Icons.send),
-              label: const Text("Envoyer"),
-              onPressed: () async {
-                Navigator.of(context).pop();
-                sendEditComment(song, comment, _commentController.text);
-                // refresh current page so posted comment is visible
-                Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => SongPageWidget(
-                            songLink: widget.songLink,
-                            song: fetchSong(widget.songLink!.id))));
-              },
-            )
-          ],
-          title: const Text('Edition d\'un commentaire'),
-          content: TextFormField(
-              maxLines: 5,
-              controller: _commentController,
-              decoration: const InputDecoration(
-                hintText: 'Entrez votre commentaire ici',
-              )),
-        );
-      },
-    );
+  void onPageChange(int page) {
+    setState(() {
+      _currentPage = page;
+    });
   }
 
   Widget _buildView(BuildContext context, Song song) {
-    final String coverLink = song.coverLink;
-    final tag = createTag(widget.songLink!);
-
-    var nestedScrollView = NestedScrollView(
-        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-          return <Widget>[
-            SliverAppBar(
-              backgroundColor: Theme.of(context).canvasColor,
-              expandedHeight: 200.0,
-              automaticallyImplyLeading: false,
-              floating: true,
-              flexibleSpace: FlexibleSpaceBar(
-                  background: Row(children: [
-                Expanded(
-                    flex: 3,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Expanded(
-                          flex: 1,
-                          child: Padding(
-                              padding: const EdgeInsets.all(5.0),
-                              child: Hero(
-                                  tag: tag,
-                                  child: Material(
-                                    child: InkWell(
-                                        onTap: () {
-                                          _openCoverViewerDialog(
-                                              widget.songLink, context);
-                                        },
-                                        child: CachedNetworkImage(
-                                            imageUrl: coverLink)),
-                                  ))),
-                        ),
-                        Expanded(
-                            flex: 1,
-                            child: SingleChildScrollView(
-                                child: SongInformations(song: song))),
-                      ],
-                    ))
-              ])),
-            ),
-          ];
-        },
-        body: Stack(children: [
-          CachedNetworkImage(
-            imageUrl: song.coverLink,
-            imageBuilder: (context, imageProvider) => Container(
-                decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: imageProvider, fit: BoxFit.cover)),
-                child: BackdropFilter(
-                  filter: ImageFilter.blur(sigmaX: 9.6, sigmaY: 9.6),
-                  child: Container(
-                    decoration: BoxDecoration(
-                        color: Colors.grey.shade200.withOpacity(0.7)),
-                  ),
-                )),
-          ),
-          Stack(
-            children: [
-              PageView(
-                controller: _pageController,
-                onPageChanged: (int page) => setState(() {
-                  _currentPage = page;
-                }),
-                children: <Widget>[
-                  song.lyrics != null ?
-                  SingleChildScrollView(
-                      child: Padding(
-                    padding: const EdgeInsets.only(left: 4.0, top: 2.0),
-                    child: HtmlWithStyle(
-                        data: song.lyrics == ''
-                            ? '<center><i>Paroles non renseignées</i></center>'
-                            : song.lyrics),
-                  )) : const Center(child: CircularProgressIndicator()),
-                  _buildViewComments(context, song),
-                ],
-              ),
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: PageIndicator(
-                  controller: _pageController,
-                  count: 2,
-                  size: 10.0,
-                  layout: PageIndicatorLayout.WARM,
-                  scale: 0.75,
-                  space: 10,
-                ),
-              ),
-            ],
-          ),
-        ]));
-
     Widget? postNewComment = Session.accountLink.id == null || _currentPage != 1
         ? null
         : FloatingActionButton(
@@ -244,42 +304,9 @@ class _SongPageWidgetState extends State<SongPageWidget> {
 
     return Scaffold(
       appBar: SongAppBar(widget.song),
-      body: nestedScrollView,
+      body: SongPageNestedScrollView(
+          widget.songLink!, song, onPageChange, _commentController),
       floatingActionButton: postNewComment,
     );
-  }
-
-  Widget _buildViewComments(BuildContext context, Song song) {
-    List<Comment> comments = song.comments;
-    var rows = <Widget>[];
-    String? loginName = Session.accountLink.name;
-    var selfComment = const TextStyle(
-      color: Colors.red,
-    );
-
-    for (Comment comment in comments) {
-      rows.add(ListTile(
-          onTap: () {
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) =>
-                        AccountPage(account: fetchAccount(comment.author.id))));
-          },
-          title: HtmlWithStyle(data: comment.body),
-          subtitle: Text('Par ${comment.author.name!} ${comment.time}',
-              style: comment.author.name == loginName ? selfComment : null),
-          trailing: comment.author.name == loginName
-              ? IconButton(
-                  icon: const Icon(Icons.edit),
-                  onPressed: () async {
-                    _editMessageDialog(context, song, comment);
-                  },
-                )
-              : null));
-      rows.add(const Divider());
-    }
-
-    return ListView(children: rows);
   }
 }
