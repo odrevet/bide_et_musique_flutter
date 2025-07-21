777ccd348ffbe5a855fda819f92367a112431ab4
odrevet <odrevet@gmail.com>
2025-07-21 07:02:30 +0200

    lint (remove unused code)

--- lib/widgets/player/song_player.dart
+++ lib/widgets/player/song_player.dart
@@ -329,13 +329,6 @@ class _SongPlayerWidgetState extends State<SongPlayerWidget>
         AudioService.position,
         (mediaItem, position) => MediaState(mediaItem, position),
       );
-
-  String _formatDuration(Duration duration) {
-    String twoDigits(int n) => n.toString().padLeft(2, '0');
-    final minutes = twoDigits(duration.inMinutes.remainder(60));
-    final seconds = twoDigits(duration.inSeconds.remainder(60));
-    return '$minutes:$seconds';
-  }
 }
 
 // MediaState class for combining media item and position
